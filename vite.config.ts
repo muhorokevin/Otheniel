882c86e927d3953155ea6d86a5e2e93f2e9ef94f
Kevin Muhoro <108815043+muhorokevin@users.noreply.github.com>
2026-01-05 15:33:12 +0300

    favicon update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   define: {
-    // This ensures process.env.API_KEY is available in the browser
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
   },
   server: {
